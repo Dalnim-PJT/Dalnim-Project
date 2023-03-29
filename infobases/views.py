583d81cec5068a1aeea5dec714eb89ba14fd18e6
sotthang <sotthang@gmail.com>
2023-03-29 23:49:11 +0900

    add weather

--- infobases/views.py
+++ infobases/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from utils.weather import weather_json
 
 # Create your views here.
 
 def main(request):
-    return render(request, 'main.html')
+    seoul = weather_json('seoul')
+
+    context = {
+        'seoul': seoul,
+        'weather': seoul['weather'][0]['description'],
+        'temp': seoul['main']['temp'],
+        'feels_like': seoul['main']['feels_like'],
+        'temp_min': seoul['main']['temp_min'],
+        'temp_max': seoul['main']['temp_max'],
+        'humidity': seoul['main']['humidity'],
+    }
+    return render(request, 'main.html', context)
+

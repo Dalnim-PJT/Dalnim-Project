7ef853787f09bd8c9e068cfc903f3471cc9b983f
sotthang <sotthang@gmail.com>
2023-05-01 23:40:48 +0900

    dev html

--- pjt_dalnim/settings.py
+++ pjt_dalnim/settings.py
@@ -124,8 +124,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'infobases/static','static')]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,'infobases/static','static')]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 

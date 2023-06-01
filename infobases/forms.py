8f34154faf6452bf29c6114b3a3484cff37b223e
sotthang <sotthang@gmail.com>
2023-06-01 23:25:56 +0900

    update profile, index, search, create

--- infobases/forms.py
+++ infobases/forms.py
@@ -2,10 +2,31 @@ from django import forms
 from .models import Info, Comment, Image
 
 class InfoForm(forms.ModelForm):
+    title = forms.CharField(
+        label='',
+        label_suffix='',
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'create--form-control',
+                'style': 'width: 100%;',
+            }
+        )
+    )
+    content = forms.CharField(
+        label='',
+        label_suffix='',
+        required=True,
+        widget=forms.Textarea(
+            attrs={
+                'class': 'create--form-control',
+                'style': 'width: 100%;',
+            }
+        )
+    )
     class Meta:
         model = Info
         fields = ('title', 'content',)
-
 
 class ImageForm(forms.ModelForm):
     class Meta:
